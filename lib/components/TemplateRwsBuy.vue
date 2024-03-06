80382a380102aa7926212aabd00e0e56574b72a2
A. Bakai @positivecrash <nastin_box@list.ru>
2024-03-06 19:59:01 +0200

    buy oage upd

--- lib/components/TemplateRwsBuy.vue
+++ lib/components/TemplateRwsBuy.vue
@@ -9,68 +9,62 @@
                     selectable
                     selectstyle
                     selectblock
-                    type="ed25519"
                 />
 
-                <robo-status v-if="expDate" :type="expStatus">Expiration date: {{expDate}}</robo-status>
+                <robo-text size="small" weight="normal-italic">
+                    To purchase or renew a subscription, you only need to have apprx. 1.5 - 2 XRT tokens in your account balance. Learn about XRT on Robonomics’ website - https://robonomics.network/xrt/
+                </robo-text>
 
-                <robo-account-polkadot-generate
-                    v-model:name="newaccountName"
-                    v-model:password="newaccountPassword"
-                    v-model:address="newaccountAddress"
-                    v-model:json="newaccountJson"
-                />
+                <template v-if="chainInfoUploaded">
+                    <robo-status v-if="expDate" :type="expStatus">Expiration date: {{expDate}}</robo-status>
 
-                <robo-section offset="x05" v-if="newaccountAddress">
-                    <robo-text size="small">You just created account: {{newaccountAddress}}</robo-text>
-                    <robo-text size="small" weight="bold">To use your account, please import it into one of the Polkadot extensions using the downloaded <a @click="downloadJson(newaccountJson, newaccountName)" href="javascript:;">JSON file</a> and the password you provided while creating</robo-text>
-                </robo-section>
+                    <robo-text size="small" weight="normal-italic">
+                        <robo-grid offset="x0" gap="x05" columns="1">
+                            <robo-grid-item borderbottom>
+                                Price from: <b>~ {{priceRound}} XRT </b> 
 
-            <robo-text size="small" weight="normal-italic">
-                <robo-grid offset="x0" gap="x05" columns="1">
-                
-                    <robo-grid-item>To purchase or renew a subscription, you only need to have apprx. 1.5 - 2 XRT tokens in your account balance. Learn about XRT on Robonomics’ website - https://robonomics.network/xrt/</robo-grid-item>
+                                <robo-details>
+                                    <template #summary><robo-icon icon="circle-question" /></template>
+                                    <robo-grid offset="x0" gap="x05">
+                                        <robo-text weight="bold">Where to buy XRT</robo-text>
+                                        <robo-link href="https://app.basilisk.cloud/pools-and-farms">Basilisk</robo-link>
+                                        <robo-link href="https://app.solarbeam.io/exchange/swap">Solarbeam</robo-link>
+                                        <robo-link href="https://trade.kraken.com/markets/kraken/xrt/usd">Kraken</robo-link>
+                                    </robo-grid>
+                                </robo-details>
+                            </robo-grid-item>
 
-                    <robo-grid-item borderbottom>
-                        Price from: <b>~ {{priceRound}} XRT </b> 
+                            <robo-grid-item borderbottom>Activation time <b>~ {{activationtime}} min</b></robo-grid-item>
 
-                        <robo-details>
-                            <template #summary><robo-icon icon="circle-question" /></template>
-                            <robo-grid offset="x0" gap="x05">
-                                <robo-text weight="bold">Where to buy XRT</robo-text>
-                                <robo-link href="https://app.solarbeam.io/exchange/swap">Solarbeam</robo-link>
-                                <robo-link href="https://trade.kraken.com/markets/kraken/xrt/usd">Kraken</robo-link>
-                            </robo-grid>
-                        </robo-details>
-                    </robo-grid-item>
-
-                    <robo-grid-item borderbottom>Activation time <b>~ {{activationtime}} min</b></robo-grid-item>
-
-                    <robo-grid-item>Available subscriptions: <b>{{available}}</b></robo-grid-item>
-                </robo-grid>
-            </robo-text>
-
-            <robo-grid-item>
-                <robo-button 
-                    @click.prevent="activateRWS()"
-                    :disabled="(status === 'processing') || expPeriod < -1"
-                    :loading="status === 'processing'"
-                    :type="buttonstatus"
-                    block
-                >
-                    {{buttontext}}
-                </robo-button>
-                
-                <robo-section offset="x05">
-                    <robo-status v-if="localerror" type="warning">
-                        {{localerror}}
-                    </robo-status>
-
-                    <robo-text v-if="message" :highlight="messagestatus">
-                        {{message}}
+                            <robo-grid-item>Available subscriptions: <b>{{available}}</b></robo-grid-item>
+                        </robo-grid>
                     </robo-text>
-                </robo-section>
-            </robo-grid-item>
+
+                    <robo-button 
+                        @click.prevent="activateRWS()"
+                        :disabled="(status === 'processing') || expPeriod < -1"
+                        :loading="status === 'processing'"
+                        :type="buttonstatus"
+                        block
+                    >
+                        {{buttontext}}
+                    </robo-button>
+
+                </template>
+
+                <template v-if="!chainInfoUploaded">
+                    <robo-text weight="normal-italic"><robo-loader size="1.5" /> Loading data from chain</robo-text>
+                </template>
+
+                <robo-status v-if="localerror" type="warning">
+                    {{localerror}}
+                </robo-status>
+
+                <robo-text v-if="message" :highlight="messagestatus">
+                    {{message}}
+                </robo-text>
+
+
         </robo-grid>
     </robo-section>
 
@@ -82,7 +76,6 @@
 
 <script setup>
 import { ref, computed, defineProps, defineEmits, onMounted, watch } from 'vue'
-import { generateAddress } from '../polkadot/tools'
 import { generateName, dateGetRange, dateGetString, setStatusView } from '../tools'
 
 import { useStore } from 'vuex'
@@ -104,9 +97,13 @@ const props = defineProps({
 
     rwsExpiration: {
         type: Number
+    },
+    chainInfoUploaded: {
+        type: Boolean
     }
 })
 
+/* округление цены подписки для красоты */
 const priceRound = computed( () => {
     let n = parseFloat(props.price)
 
@@ -118,21 +115,7 @@ const priceRound = computed( () => {
 })
 
 
-/* + Processing */
-const status = ref(null) // ok, error
-const message = ref(null) // string
-const localerror = ref(null)
-
-const emit = defineEmits(['onActivate'])
-
-let newacc = () => {
-    const { mnemonic, json } = generateAddress()
-    return [json.address, mnemonic]
-}
-
-// const rwsExpiration = computed( () => {
-//   return store.state.robonomicsUIvue.rws.expiredate
-// })
+/* + получение даты окончание подписки */
 
 const expPeriod = computed( () => {
    if(props.rwsExpiration) {
@@ -159,16 +142,43 @@ const expStatus = computed( () => {
     }
 })
 
-const defaultButtonText = computed( () => {
-    if(props.rwsExpiration) {
-        if( expPeriod.value < -1 ) {
-            return 'Your subscription is active'
-        } else {
-            return 'Renew subscription'
-        }
-    } else {
-        return 'Buy subscription'
-    }
+/* - получение даты окончание подписки */
+
+
+/* + работа с текстом кнопки */
+const defaultbuttontext = computed( () => {
+    if(!props.rwsExpiration) return 'Buy subscription'
+    return props.rwsExpiration < -1 ? 'Your subscription is active' : 'Renew subscription'
+})
+
+const buttontext = computed( () => {
+    return ({
+    'ok': 'RWS activated',
+    'error': 'Something went wrong'
+    }[status.value] ?? defaultbuttontext.value)
+})
+
+/* - работа с текстом кнопки */
+
+/* + Status handling, bying */
+const status = ref(null) // ok, error
+const message = ref(null) // string
+const localerror = ref(null)
+const emit = defineEmits(['onActivate'])
+
+const buttonstatus = computed( () => {
+    return ({
+    'ok': 'ok',
+    'error': 'error'
+    }[status.value] ?? 'primary')
+})
+
+const messagestatus = computed( () => {
+    return ({
+    'ok': 'ok',
+    'error': 'error',
+    'new': 'attention'
+    }[status.value] ?? null)
 })
 
 let activateRWS = () => {
@@ -199,6 +209,9 @@ let setRwsAddressActive = () => {
         store.commit('polkadot/setAddress', rws)
     }
 }
+/* - Status handling, bying */
+
+
 
 onMounted( () => {
 
@@ -213,21 +226,18 @@ onMounted( () => {
     })
 
     watch( () => status.value, status => {
+
         if(status === 'ok') {
 
-            if(props.rwsExpiration) {
-                // если подписка существует ??
-            }
-            else {
-                /* Generates user, controller and name; */
-                const owner = store.state.robonomicsUIvue.polkadot.address
-                const controller = newacc()
-                const controlleraddr = controller[0]
-                const controllerseed = controller[1]
-                const name = generateName()
+            const owner = store.state.robonomicsUIvue.polkadot.address
 
-                store.dispatch('rws/create', { owner, controller: controlleraddr, controllerseed, name })
-            }
+            store.dispatch('rws/findrws', owner).then( index => {
+                if(index < 0) {
+                    // generate new settings
+                    const name = generateName()
+                    store.dispatch('rws/create', { owner, name })
+                }
+            })
 
             // обнуляем статус
             setTimeout( () => {
@@ -242,34 +252,5 @@ onMounted( () => {
 
 })
 
-const buttontext = computed( () => {
-    return ({
-    'ok': 'RWS activated',
-    'error': 'Something went wrong'
-    }[status.value] ?? defaultButtonText.value)
-})
 
-const buttonstatus = computed( () => {
-    return ({
-    'ok': 'ok',
-    'error': 'error'
-    }[status.value] ?? 'primary')
-})
-
-const messagestatus = computed( () => {
-    return ({
-    'ok': 'ok',
-    'error': 'error',
-    'new': 'attention'
-    }[status.value] ?? null)
-})
-/* - Processing */
-
-/* + Generate new account */
-import { downloadJson } from '../tools'
-let newaccountName = ref(null)
-let newaccountPassword = ref(null)
-let newaccountAddress = ref(null)
-let newaccountJson = ref(null)
-/* - Generate new account */
 </script>
